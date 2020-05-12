05020066fa48a004d0fbc08b08322878cafa6b77
Wilson Tu <s31302@gmail.com>
2020-05-12 15:56:58 -0700

    Documentation added

--- website updates/updateScript.py
+++ website updates/updateScript.py
@@ -1,32 +1,23 @@
-# Import requests (to download the page)
-import requests
+"""Monitors the given course and send update when a spot frees up
 
-# Import BeautifulSoup (to parse what we download)
+    Usage: change the url to the course, chagne the word looking for to however, many people is registered in the class
+    , subscribe to the printed website to recieve notification
+"""
+
+
 from bs4 import BeautifulSoup
-
-# Import Time (to add a delay between the times the scape runs)
 import time
-
 from urllib.request import urlopen
-
-# Import smtplib (to allow us to email)
-import smtplib
-
 import win32api
 from notify_run import Notify
 
-# This is a pretty simple script. The script downloads the homepage of VentureBeat, and if it finds some text, emails me.
-# If it does not find some text, it waits 60 seconds and downloads the homepage again.
+#Creates the notify channel
 notify = Notify()
 print(notify.register())
-# while this is true (it is true by default),
+## Keeps looping through the website until a spot is open
 while True:
-    # set the url as VentureBeat,
     url = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=BIOL&course=234&section=921"
-    # set the headers like we are a browser,
     html = urlopen(url).read()
-    # download the homepage
-    # parse the downloaded homepage and grab all text, then,
     soup = BeautifulSoup(html, "html.parser")
     text = soup.get_text()
     text_list = text.split()
@@ -34,17 +25,16 @@ while True:
     word_looking_for = "Registered:100"
     
 
-    # if the number of times the word "Google" occurs on the page is less than 1,
+    # if the amount of people registered has not changed keep looping
     if word_looking_for in text_list:
         # wait 60 seconds,
         time.sleep(60)
         # continue with the script,
         continue
         
-    # but if the word "Google" occurs any other number of times,
+    # if the amout of people registered has changed do a pop up and send a notificaiton on the website
     else:
         full_text = soup.find_all("Registered:100")
         notify.send("register for course now")
         win32api.MessageBox(0, 'BIOL234', 'Course registeration')
-        
         break
